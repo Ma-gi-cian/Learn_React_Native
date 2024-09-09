3f2245f1f41dd08c5e46dda14419b94cb504e143
Aditya Jha <aditya01jha.02@gmail.com>
2024-09-09 23:52:50 +0530

    first commit : A working react native proj

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./components/**/*.{js,jsx,ts,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
